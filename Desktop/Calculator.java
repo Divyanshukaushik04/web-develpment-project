92149030533f9a1ee34c9c9c4c6571d571adb0ab
Divyanshu Kaushik <divyanshu2438.be21@chitkara.edu.in>
2024-10-16 10:54:35 +0530

    Added: Addition Functiality is Added

---
+++ Desktop/Calculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Calculator{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+int a = 10;
+int b = 20;
+int c = a+b;
+
+
+System.out.print(c);
+}
+
+}
